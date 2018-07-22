fa4ecd92a045a55bfbdc147de981a4664bd9976b
Jeroen Meulemeester <jeroen.meulemeester@gmail.com>
2018-07-22 15:08:22 +0200

    Replace memory mapped registers with structs

--- main.c
+++ main.c
@@ -4,21 +4,21 @@
 
 void SystemInit(void) {
 	// Enable clock for IOPC
-	RCC_APB2ENR |= 1 << 4;
+	RCC.apb2enr |= 1 << 4;
 
 	// Configure GPIOC_13 as output:
 	//   - MODE = 11 (output, 50Mhz)
 	//   - CNF  = 01 (open-drain)
-	unsigned long gpioc_config = GPIOC_CRH;
+	unsigned long gpioc_config = GPIOC.crh;
 	gpioc_config &= ~(0xff << 20);
 	gpioc_config |= 0x07 << 20;
-	GPIOC_CRH = gpioc_config;
+	GPIOC.crh = gpioc_config;
 }
 
 void main(void) {
 	volatile unsigned long i;
 	while(1) {
-		GPIOC_ODR ^= 1 << 13;
+		GPIOC.odr ^= 1 << 13;
 		for(i=0; i<DELAY; i++);
 	}
 }
--- stm32f1.h
+++ stm32f1.h
@@ -1,13 +1,69 @@
 #ifndef STM32F1_H
 #define STM32F1_H
 
-#define RCC_BASE	0x40021000
-#define RCC_APB2ENR	(*(volatile unsigned long*) (RCC_BASE + 0x18))
+/*
+ * General purpose IO registers
+ */
+struct reg_gpio {
+	volatile unsigned long crl;
+	volatile unsigned long crh;
+	volatile unsigned long idr;
+	volatile unsigned long odr;
+	volatile unsigned long bssr;
+	volatile unsigned long brr;
+	volatile unsigned long lckr;
+};
 
-#define GPIOC_BASE	0x40011000
-#define GPIOC_CRL	(*(volatile unsigned long*) (GPIOC_BASE + 0x00))
-#define GPIOC_CRH	(*(volatile unsigned long*) (GPIOC_BASE + 0x04))
-#define GPIOC_ODR	(*(volatile unsigned long*) (GPIOC_BASE + 0x0c))
-#define GPIOC_BSRR	(*(volatile unsigned long*) (GPIOC_BASE + 0x10))
+/*
+ * Memory mapped general purpose IO registers struct
+ */
+#define GPIOA	(*(struct reg_gpio *) 0x40010800)
+#define GPIOB	(*(struct reg_gpio *) 0x40010C00)
+#define GPIOC	(*(struct reg_gpio *) 0x40011000)
+#define GPIOD	(*(struct reg_gpio *) 0x40011400)
+#define GPIOE	(*(struct reg_gpio *) 0x40011800)
+#define GPIOF	(*(struct reg_gpio *) 0x40011C00)
+#define GPIOG	(*(struct reg_gpio *) 0x40012000)
+
+/*
+ * Alternate function IO registers
+ */
+struct reg_afio {
+	volatile unsigned long evcr;
+	volatile unsigned long mapr;
+	volatile unsigned long exticr1;
+	volatile unsigned long exticr2;
+	volatile unsigned long exticr3;
+	volatile unsigned long exticr4;
+	volatile unsigned long mapr2;
+};
+
+/*
+ * Memory mapped alternate function IO registers struct
+ */
+#define AFIO	(*(struct reg_afio *) 0x40010000)
+
+/*
+ * Reset and clock control registers
+ */
+struct reg_reset_clock_control {
+	volatile unsigned long cr;
+	volatile unsigned long cfgr;
+	volatile unsigned long cir;
+	volatile unsigned long apb2rstr;
+	volatile unsigned long apb1rstr;
+	volatile unsigned long ahbenr;
+	volatile unsigned long apb2enr;
+	volatile unsigned long apb1enr;
+	volatile unsigned long bdcr;
+	volatile unsigned long csr;
+	volatile unsigned long ahbstr;
+	volatile unsigned long cfgr2;
+};
+
+/*
+ * Memory mapped reset and clock control registers struct
+ */
+#define RCC	(*(struct reg_reset_clock_control *) 0x40021000)
 
 #endif
